277a6cbe6db1c667c460840a45086ef86584e99e
Susan Pallmann <38414619+susanpallmann@users.noreply.github.com>
2020-06-04 14:35:59 -0400

    Update ui.js

--- ui.js
+++ ui.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
     $('#submit-key').submit(function(event) {
-        var key = $('room-key').val();
+        var key = $('#room-key').val();
         var keyLength =  key.length;
         if (keyLength < 4 || keyLength > 4) {
             $('#entering-key').append('<p class="error">Please enter a valid room key, must be 4 letters.</p>');
