8e417687e5c9deec33fe2f526407b51d7259d7a0
Susan Pallmann <38414619+susanpallmann@users.noreply.github.com>
2020-06-05 13:19:10 -0400

    Update monsters.js

--- monsters.js
+++ monsters.js
@@ -1,3 +1,3 @@
 var monsters = [
-    {id: 0001, name: "Skeleton", sprite: "images/sprites/monsters/skeleton.png", level: 3, effect: "none", description: "Example text of a description.", loot: 3},
+    {name: "Skeleton", sprite: "images/sprites/monsters/skeleton.png", description: "Example text of a description."},
 ]
