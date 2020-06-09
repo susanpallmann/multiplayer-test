38a406f070399adab7f314e5e3a057957ce0ff50
Susan Pallmann <38414619+susanpallmann@users.noreply.github.com>
2020-06-09 17:55:52 -0400

    Update cards.js

--- cards.js
+++ cards.js
@@ -1,3 +1,3 @@
 var deck = [
-    {}
+    {id: 0, type: "", title: "example title", desc: "This is an example description; it tells you a tiny bit about this item.", val: 4, effect: "example"}  
 ];
