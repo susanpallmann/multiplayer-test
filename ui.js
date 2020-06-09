e919e9f40a8afc35cb1dadd36387251b7bae4063
Susan Pallmann <38414619+susanpallmann@users.noreply.github.com>
2020-06-08 21:04:06 -0400

    Update ui.js

--- ui.js
+++ ui.js
@@ -3,29 +3,6 @@ $(document).ready(function() {
         generateRoomCode();
         event.preventDefault();
     });
-    $('#enter-room').submit(function(event) {
-        var key = $('#room-key').val();
-        var keyLength =  key.length;
-        if (keyLength < 4 || keyLength > 4) {
-            $('#enter-room').append('<p class="error">Please enter a valid room key, must be 4 letters.</p>');
-        } else {
-            $('.error').remove();
-            console.log(key);
-            var values = {
-                numPlayers: 0,
-                roomKey: key,
-                player1: null,
-                player2: null,
-                player3: null,
-                player4: null
-            }
-            var pathRef = firebase.database().ref('games/' + key);
-            var newChildRef = pathRef.set(values);
-        }
-        updateGameSetup(key);
-        updatePlayers(key);
-        event.preventDefault();
-    });
     $('#enter-game').submit(function(event) {
         var key = $('#my-room-key').val();
         var keyLength = key.length;
@@ -130,30 +107,79 @@ function setupPlaySpace(key, user, num) {
 }
 // Function to generate a random room code for the start of the game.
 function generateRoomCode() {
+    // Starts with an empty string.
     var roomCode = "";
+    // Recursive function to check if the room code is complete and generate random letters if not.
     function generateDigits(code) {
+        // If the string isn't yet 4 characters long
         if (code.length < 4) {
+            // Generate a random number between 0 and 25
             var tempNum = Math.floor(Math.random() * 25);
+            // Convert this new value to an ascii character (uppercase)
             var tempLetter = String.fromCharCode(65 + tempNum);
+            // Add the new value to the existing room code
             roomCode =  code + tempLetter;
             console.log(roomCode);
+            // Run this function again to check if the code is complete (length of 4)
             generateDigits(roomCode);
+        // If the string is 4 characters
         } else {
+            // End function
             return code;
         }
     }
+    // Calls the previous function the first time it's run.
     generateDigits(roomCode);
     console.log(roomCode);
+    // Passes the finalized code into the verifyRoomCode function
     verifyRoomCode(roomCode);
 }
+// Function to check if the room key passed into it (key) is already an in-session game in the database
 function verifyRoomCode(key) {
+    // Checks that specific location in the database and takes a snapshot
     firebase.database().ref('games/' + key).once("value", snapshot => {
+        // If the snapshot exists already
         if (snapshot.exists()) {
+            // Rerun the code generator and try again
+            // While this method isn't entirely as efficient as it could be, this is currently only made for a few close friends to be using, 
+            // so it is statistically unlikely that that the function will need run again
+            // ¯\_(ツ)_/¯
             generateRoomCode();
             return true;
+        // If the snapshot doesn't exist, we can set up the lobby
         } else {
+            // Calls function lobbySetup
+            lobbySetup(key);
             console.log("We will set up the game by calling a function from here.");
             return false;
         }
     });
+}
+// Function to set up the initial lobby for users to join client-side.
+function lobbySetup(key) {
+    // Sets up some dummy directory values and sets some mechanical numbers
+    var values {
+        roomCode: key, // Room code, just in case it's needed
+        playerCount: 0, // Keeps a count of how many players are in game
+        phase: 1, // Tracks game progression, see below:
+            // Phase 1: Lobby || Users can join from their phones, transitions once the VIP presses "everyone's in."
+            // Phase 2: Setup || Deals cards to users, introduces instructions, transitions on a timer.
+            // Phase 3: Play  || Gameplay, the main part of the game, transitions when either someone wins or there are too few players.
+            // Phase 4: Win   || Shows a winner and overall scoreboard, ends play, and allows either repeat game or a full restart.
+            // Phase 5: Error || Ends the game due to too few players, shows error on screen and then brings host to the homepage.
+            // Phase 6: Close || Occurs if the host window is closed, ends the game and removes the room code again.
+        // Player slots
+        player1: null,
+        player2: null,
+        player3: null,
+        player4: null,
+        player5: null,
+        player6: null,
+        player7: null,
+        player8: null,
+    };
+    // Sends new game to the database stored under the room key.
+    var ref = firebase.database().ref('games/' + key);
+    var newChildRef = ref.set(values);
+    // TODO: Initialize some trackers now that we have a room code (like game phase)
 }
