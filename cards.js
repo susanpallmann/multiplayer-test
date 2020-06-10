131850b8dba6f8ec840af47faef77a3343dc42c7
Susan Pallmann <38414619+susanpallmann@users.noreply.github.com>
2020-06-10 12:22:41 -0400

    Update cards.js

--- cards.js
+++ cards.js
@@ -1,4 +1,4 @@
 var deck = [
-    {id: 0, type: "card", score: -2, title: "Curse of Weakness", desc: "Your victim feels like their life force has somehow drained a bit.", val: 2, effect: "none"},
-    {id: 1, type: "item", score: 4, title: "Bone Sword of Victory", desc: "... That's pretty gross, actually.", val: 4, effect: "none"}
+    {id: 0, type: "card", sprite:"potion", score: -2, title: "Curse of Weakness", desc: "Your victim feels like their life force has somehow drained a bit.", val: 2, effect: "none"},
+    {id: 1, type: "item", sprite:"bone-sword", score: 4, title: "Bone Sword of Victory", desc: "... That's pretty gross, actually.", val: 4, effect: "none"}
 ];
