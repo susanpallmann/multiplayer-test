73370c124125b9092fba20e600cd79eee054f122
Susan Pallmann <38414619+susanpallmann@users.noreply.github.com>
2020-06-10 12:52:44 -0400

    Update client-side.js

--- client-side.js
+++ client-side.js
@@ -349,8 +349,8 @@ function initiateProfile(key) {
     });
 }
 // Function to edit inventory items (not equipped items for a specific player)
-function updateItem(key, player, id, bool) {
-    var playerRef = firebase.database().ref('games/' + key + '/' + player + '/items');
+function updateItem(key, location, id, bool) {
+    var playerRef = firebase.database().ref('games/' + key + '/' + location);
     // A boolean is passed in to determine if adding or removing the specified item (true=add, false=delete)
     if (bool) {
         var values = {};
